9435478b859632255b2f8f936a939d93b3ee6709
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-13 12:45:48 +0100

    notes added to js file of intentions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// dire roll function
+// vibrate the dice for a random number of seconds
+// display one of the 6 dice faces
+// check if a 1 has been rolled
+// if so - display You Lose
+// add the dice roll to the score variable
+
+
+// check for a winner function
+// if score > 20 display Your Win
+// if score <= 20 continue
+
+// reset game function
+// reset score to zero
+// display the 3d dice ready to play
